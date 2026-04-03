87736c2a64ae65a911fa89877e7b48932f7cf248
malu-website <info@malu2go.de>
2026-04-03 19:36:22 +0200

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,10 +1,5 @@
 import logo from './logo.png'
-const cardStyle = {
-  padding: "20px",
-  border: "1px solid #ddd",
-  borderRadius: "16px",
-  background: "#f9f7f3"
-}
+
 const benefits = [
   {
     title: 'Hochwertiger Auftritt für Events',
